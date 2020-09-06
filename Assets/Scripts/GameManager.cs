1591ef8c435fda261163d71878968d618e7b91cf
Bowie8122 <sintlucasbowie@gmail.com>
2020-09-06 14:44:21 +0200

    Bullet
    Extra Stuff :)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void MyDelegate();
+    public static MyDelegate Bullets;
+    
+    public void SpawnBullet()
+    {
+        Debug.Log("Spawn Bullet");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SpawnBullet();
+        }
+    }
+}
---
+++ Assets/Scripts/IConstantmove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IConstantmove
+{
+    float Speed
+    {
+        get; set;
+    }
+    void Move(float speed);
+}
