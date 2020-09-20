9dbbbc9d011411374551c17a1cc71379cc671970
PepijnvanBergen <pepijn0328@gmail.com>
2020-09-20 23:36:43 +0200

    playermovement ree en bullet gefikst

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -10,13 +10,33 @@ public class InputManager
     private bool _runInput;
 
     // Update is called once per frame
+/*
+Wat we moeten doen
+
+Input manager, die moet input geven, die een event aanzet; MoveL, MoveR
+
+PlayerMovement, luisteren naar de event, en dan links of rechts gaan.
+
+Probleem, ik wil ook rennen dus ik moet of 4 events en 4 states en 4 functies gaan maken voor de movement.
+Wat kapot omslachtig is.
+
+Of ik moet een andere mannier vinden.
+
+
+
+
+*/
     public void InputUpdate()
     {
         _shootInput = Input.GetKeyDown(KeyCode.Space);
         _runInput = Input.GetKeyDown(KeyCode.LeftControl);
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            EventManager.RaiseEvent(EventEnum.ON_MOVE);
+            EventManager.RaiseEvent(EventEnum.ON_MOVEL);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            EventManager.RaiseEvent(EventEnum.ON_MOVER);
         }
         if (_shootInput)
         {
